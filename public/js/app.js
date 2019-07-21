4837feb499fad882229159159f17009d1e95e819
YUZHU CHAI <yuzhupoemhead@gmail.com>
2019-07-21 15:38:54 -0500

    video streaming working

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+console.log("linked to html");
+
+const video = document.querySelector("#videoElement");
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
+}
